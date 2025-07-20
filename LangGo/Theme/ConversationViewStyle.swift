0a8bfc387e959d149423905930d923197983f27f
jamestjtang <juntjtang@163.com>
2025-07-20 02:54:46 -0700

    Change style a little bit for ConversationView.

--- LangGo/Theme/ConversationViewStyle.swift
+++ LangGo/Theme/ConversationViewStyle.swift
@@ -17,7 +17,7 @@ struct ConversationStyleModifier: ViewModifier {
         switch style {
         case .messageBubble(let isUser):
             content
-                .font(.system(size: 15)) // Set font size to 15pt
+                .font(.system(size: 16)) // Set font size to 15pt
                 .padding()
                 .background(isUser ? theme.primary.opacity(0.8) : theme.secondary.opacity(0.8))
                 .foregroundColor(theme.text)
@@ -25,8 +25,8 @@ struct ConversationStyleModifier: ViewModifier {
 
         case .micButton(let isListening):
             content
-                .font(.system(size: 41)) // Reduced font size
-                .frame(width: 86, height: 86) // Reduced frame size by ~20%
+                .font(.system(size: 36)) // Reduced font size
+                .frame(width: 76, height: 76) // Reduced frame size by ~20%
                 .background(isListening ? Color.red.opacity(0.8) : theme.accent)
                 .foregroundColor(.white)
                 .clipShape(Circle())
